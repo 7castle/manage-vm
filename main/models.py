74667a57fe98de6afab387fc2abd0ebb82d5f083
zujko <peter@zujko.xyz>
2016-03-19 00:10:50 -0400

    Created initial Disk, NIC, and VM models

--- main/models.py
+++ main/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Disk(models.Model):
+  storage_domain = models.CharField(max_length=100)
+  size = models.IntegerField()
+  status = models.CharField(max_length=20, null=True)
+  interface = models.CharField(max_length=50)
+  format = models.CharField(max_length=50)
+  bootable = models.BooleanField()
+
+class NIC(models.Model):
+  name = models.CharField(max_length=20)
+  network = models.CharField(max_length=50)
+  interface = models.CharField(max_length=20)
+
+class VM(models.Model):
+  user = models.ForeignKey(User, related_name='user', null=False)
+  name = models.CharField(max_length=100)
+  description = models.CharField(max_length=200)
+  memory = models.IntegerField()
+  cores = models.IntegerField()
+  cluster = models.CharField(max_length=100)
+  template = models.CharField(max_length=100)
+  disk = models.ForeignKey(Disk, related_name='disk', null=False)
+  nic = models.ForeignKey(NIC, related_name='nic', null=False) 
