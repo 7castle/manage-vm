15c2aca901d4719765a85b214c49fce61f232f9d
zujko <peter@zujko.xyz>
2016-03-19 19:14:37 -0400

    Implemented more view logic

--- main/views.py
+++ main/views.py
@@ -5,6 +5,32 @@ def index(request):
     return render(request, 'index.html')
 
 def create_vm(request):
-  vm_form = VM_Form()
-  disk_form = Disk_Form()
+  if request.method == 'POST':
+    vm_form = VM_Form(data=request.POST)
+    disk_form = Disk_Form(data=request.POST)
+
+    if vm_form.is_valid() and disk_form.is_valid():
+      disk = disk_form.save(commit=False)
+      disk.storage_domain = ''
+      disk.status = None
+      disk.interface = ''
+      disk.save()
+      
+      nic = NIC()
+      nic.name = ''
+      nic.network = ''
+      nic.interface = ''
+      nic.save()
+
+      vm = vm_form.save(commit=False)
+      vm.user = request.user
+      vm.cluster = ''
+      vm.disk = disk
+      vm.nic = nic
+      vm.save()
+
+  else:
+    vm_form = VM_Form()
+    disk_form = Disk_Form()
+  
   return render(request, 'create.html',{'vm_form': vm_form, 'disk_form': disk_form})
