f8cc9544bca44f97dfaab54df9ae25f8ac5ec91f
zujko <zujko@csh.rit.edu>
2016-03-30 18:38:06 -0400

    Update models

--- main/models.py
+++ main/models.py
@@ -1,36 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Disk(models.Model):
-  disk_name = models.CharField(max_length=20)
-  storage_domain = models.CharField(max_length=100)
-  size = models.IntegerField()
-  status = models.CharField(max_length=20, null=True)
-  interface = models.CharField(max_length=50)
-  format = models.CharField(max_length=50)
-  bootable = models.BooleanField()
-
-  def __unicode__(self):
-    return u'%s' % self.disk_name
-
-class NIC(models.Model):
-  name = models.CharField(max_length=20)
-  network = models.CharField(max_length=50)
-  interface = models.CharField(max_length=20)
-  
-  def __unicode__(self):
-    return u'%s' % self.name
-
 class VM(models.Model):
   user = models.ForeignKey(User, related_name='user', null=False)
-  vm_name = models.CharField(max_length=30)
-  description = models.CharField(max_length=200)
-  memory = models.IntegerField()
-  cores = models.IntegerField()
-  cluster = models.CharField(max_length=100)
+  vmid = models.PositiveIntegerField()
   template = models.CharField(max_length=100)
-  disk = models.ForeignKey(Disk, related_name='disk', null=False)
-  nic = models.ForeignKey(NIC, related_name='nic', null=False)
+  hostname = models.CharField(max_length=30)
+  storage = models.CharField(max_length=50)
+  memory = models.PositiveIntegerField()
+  swap = models.PositiveIntegerField()
+  cores = models.PositiveSmallIntegerField()
+  disk = models.PositiveIntegerField()
+  description = models.CharField(max_length=200)
+  ip = models.CharField(max_length=15)
 
   def __unicode__(self):
-    return u'%s' % self.vm_name
+    return u'%s' % self.hostname
