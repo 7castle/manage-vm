09f85890d38994953fc654a3e3294c9b84f89320
zujko <zujko@csh.rit.edu>
2016-04-03 20:42:57 -0400

    Added proxmox variables to secrets template and stubbed out creating an openvz container

--- main/views.py
+++ main/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .forms import *
+from proxmoxer import ProxmoxAPI
+from managevm import secrets
 
 def index(request):
   return render(request, 'index.html')
@@ -16,6 +18,22 @@ def create_vm(request):
       vm = vm_form.save(commit=False)
       vm.user = request.user
       vm.save()
+
+      proxmox = ProxmoxAPI(secrets.PROXMOX_HOST,user=secrets.PROXMOX_USER,password=secrets.PROXMOX_PASS,verify_ssl=True)
+
+      node = proxmox.nodes('test_node')
+
+      # [TESTING] Create openvz container 
+      node.openvz.create(vmid=202,
+                        hostname=vm_form.cleaned_data['hostname'],
+                        storage='local',
+                        memory=vm_form.cleaned_data['memory'],
+                        swap=vm_form.cleaned_data['swap'],
+                        cpus=vm_form.cleaned_data['cores'],
+                        disk=vm_form.cleaned_data['disk'],
+                        password=secrets.PROXMOX_PASS,
+                        ip_address='0.0.0.0')
+
   else:
     vm_form = VM_Form()
   
